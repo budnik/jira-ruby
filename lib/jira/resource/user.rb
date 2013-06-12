59cdb821aded6697578702feb2b7b610196a398d
Alexander Trigub <altr@ciklum.com>
2013-06-12 15:12:59 +0300

    add assignable search for User

--- lib/jira/resource/user.rb
+++ lib/jira/resource/user.rb
@@ -8,6 +8,16 @@ module JIRA
       def self.singular_path(client, key, prefix = '/')
         collection_path(client, prefix) + '?username=' + key
       end
+
+      def self.find_by_project(client, key, prefix = '/')
+        url = collection_path(client, prefix) + "/assignable/search?project=" + key
+        response = client.get(url)
+        json = parse_json(response.body)
+        json.map do |user|
+          client.User.build(user)
+        end
+      end
+
     end
 
   end
